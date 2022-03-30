3838e55428fcfb737810d6d6550bf9df7774cc94
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-03-30 11:32:02 -0300

    aula de css modules

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -2,21 +2,24 @@ import "./App.css"
 import HelloWorld from "./components/Aula01/HelloWorld"
 import Frase from "./components/Aula01/Frase"
 import Props from "./components/Aula02/Props"
+import Test from "./components/Aula03/Test"
+
 
 function App() {
-  const image = "https://via.placeholder.com/250"
   return (
     <div className="App">
       <div>
         <h1>Aula 01</h1>
         <HelloWorld />
-        <img src={image} alt="Imagem Qualquer" />
         <Frase name="Marcos" />
       </div>
       <div>
         <h1>Aula 02</h1>
         <Props name="Mateus Azevedo" age={22} country="Rio de Contas-BA" />
-        
+      </div>
+      <div> {/* Aula 03 */}
+        <h1>Aula 03</h1>
+        <Test />
       </div>
     </div>
   )
---
+++ my-project/src/components/Aula03/Test.jsx
@@ -0,0 +1,16 @@
+import styles from './Test.module.css'
+
+function Test() {
+    return (
+        <div className={styles.testContainer}>
+            <h2 className={styles.testContent}>Esse é um teste para Css Modules</h2>
+            <ul>
+                <li><a className={styles.testContent} href="#">Home</a></li>
+                <li><a className={styles.testContent} href="#">About Us</a></li>
+                <li><a className={styles.testContent} href="#">Contact Us</a></li>
+            </ul>
+        </div>        
+    )
+}
+
+export default Test
