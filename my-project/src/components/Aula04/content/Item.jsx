32f4f0d0d6486d62d2ea9f318b007011a29b3a8c
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-03-30 13:10:26 -0300

    aula de props avançado

--- my-project/src/components/Aula04/content/Item.jsx
+++ my-project/src/components/Aula04/content/Item.jsx
@@ -1,12 +1,24 @@
+import PropTypes from 'prop-types'
+
 function Item({ brand, year }) {
     return (
         <>
             <li>
                 <h2>{brand}</h2>
-                <strong>{year}</strong>
+                <p>{year}</p>
             </li>
         </>
     )
+}
+
+Item.propTypes = {
+    brand: PropTypes.string.isRequired,
+    year: PropTypes.number
+}
+
+Item.defaultProps = {
+    brand: 'Sem marca definida',
+    year: 0
 }
 
 export default Item
