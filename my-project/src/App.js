ca0abdd82f89af2c4899dd5e30657146d45dbeee
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-03-31 15:59:19 -0300

    chore: refatoring App.js

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import EventoProps from "./components/Aula06/EventoProps";
-function App() {
-  return (
-    <div className="App">
-      <EventoProps />
-    </div>
-  );
-}
+
+const App = () =>
+
+<div className="App">
+    <EventoProps />
+  </div>
+
+
+
+
 
 export default App;
